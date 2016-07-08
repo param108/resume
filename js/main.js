9a3bc61730aefbf3acf651b7651cb244dbf4fe6e
paramp <paramp@cisco.com>
2016-07-08 14:56:39 +0530

    Timeline working.

--- js/main.js
+++ js/main.js
@@ -8,12 +8,18 @@ window.jQuery = require('jquery');
 window.$=window.jQuery;
 var Bootstrap = require('bootstrap');
 var Loader=require('./loading.js');
+var Timeline = require('./timeline.js');
 
 $( document ).ready(function(){
 console.log("Running");
-ReactDOM.render(
+/*ReactDOM.render(
   <Loader max="100" val="16" inc="10"/>,
-  document.getElementById('example')
+  document.getElementById('loading')
+);*/
+
+ReactDOM.render(
+  <Timeline start="2000" end="2016" pos="2016"/>,
+  document.getElementById('timeline')
 );
 });
 
--- js/timeline.js
+++ js/timeline.js
@@ -11,46 +11,46 @@ var Dispatch = assign({}, Dispatcher.prototype,{});
 <div class="scale1"><img src="pic/scale.png"/><span>2001</span></div><div class="scale2"><img src="pic/scale.png"/><span>2001</span></div><div class="scale3"><img src="pic/scale.png"/><span>2001</span></div><div class="scale4"><img src="pic/scale.png"/><span>2001</span></div><div class="scale5"><img src="pic/scale.png"/><span>2001</span></div><div class="scale6"><img src="pic/scale.png"/><span>2001</span></div><div class="scale7"><img src="pic/scale.png"/><span>2001</span></div><div class="scale8"><img src="pic/scale.png"/><span>2001</span></div><div class="scale9"><img src="pic/scale.png"/><span>2001</span></div>
 </div>
 */
-var Loading= React.createClass({
+/*
+ * we render the line only once 
+ */
+var _lineshown = false;
+var Timeline= React.createClass({
   getInitialState: function() {
     return ({
-            start: this.props.start,
-            end: this.props.max,
-            pos: this.props.inc
+            start: parseInt(this.props.start),
+            end: parseInt(this.props.end),
+            pos: parseInt(this.props.pos)
            });
   },
   render: function() {
+    var lineml = []
+    for (var i = this.props.start; i <= this.props.end; i++) {
+      classname="scale scnum"+i.toString();
+      keyname="scalekey"+i.toString();
+      pos = (10 + 50*(i - this.props.start)).toString()+"px";
+      style= { left: pos };
+      lineml.push(
+        <div key={keyname} className={classname} style={style}><img src="pic/scale.png"></img><span>{i}</span></div>
+      );
+    }
     val = this.state.val;
     extraclass = "";
     if (val < 16) {
       extraclass = " blacktext";
     }
     extraclass="progress-bar"+extraclass;
+    indicatorstyle={ left: (10 + 50*(this.state.pos - this.state.start)) };
     return (
-    <div>
-    <div className="loading-wrapper-div">
-    </div>
-    <div className="loading-div">
-      <div className={extraclass} role="progressbar" aria-valuenow="70"
-           aria-valuemin="0" aria-valuemax="100" style={{width:val+"%"}}>
-         <span>loading...{val}%</span>
-      </div>
-    </div>
+    <div className="wholescale">
+      <div style={indicatorstyle} className="indicator"><img src="pic/indicator.png"/></div>
+      {lineml}
     </div>
     );
   },
-  updateloading(data) {
-    var newval = (parseInt(this.state.val) + parseInt(this.state.inc));
-    if (newval > 100) {
-      newval = 100;
-      data.endfn();
-    }
-    this.setState({val: newval.toString()});
-  },
   componentDidMount: function() {
-    Dispatch.register('INCREMENT_LOADING', this.updateloading);
   },
 
 });
 
-module.exports=Loading;
+module.exports=Timeline;
