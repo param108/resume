15cc3ba3cc6ff20fc56c5a7b9aa2559b63f32685
paramp <paramp@cisco.com>
2016-07-24 19:15:35 +0530

    Refactoring. Calculate the position before calling render.

--- js/timeline.js
+++ js/timeline.js
@@ -17,23 +17,28 @@ var Dispatch = assign({}, Dispatcher.prototype,{});
 var _lineshown = false;
 var Timeline= React.createClass({
   getInitialState: function() {
+    start = parseInt(this.props.start);
+    pos = parseInt(this.props.pos);
+    end = parseInt(this.props.end);
+    width = $(window).width();
+    ret = this.updatePosition("resize", start, end, pos, width);
     return ({
-            start: parseInt(this.props.start),
-            end: parseInt(this.props.end),
-            pos: parseInt(this.props.pos),
-            width: $(window).width(),
-            trigger: "resize"
+            start: start,
+            end: end,
+            pos: pos,
+            width: width,
+            coords: ret
            });
   },
-  updatePosition: function() {
+  updatePosition: function(trigger, start, end, pos, width) {
     /*
      * Extra 1 is for the indicators
      */
-    margin = (this.state.width - ((((this.state.end - this.state.start) + 1 + 1)*50)))
+    margin = (width - ((((end - start) + 1 + 1)*50)))
     var ret = {};
     margin = margin/2;
     if (margin > 0) {
-      var rightarrowpos = ((this.state.end - this.state.start) + 1)*50 + 25;
+      var rightarrowpos = ((end - start) + 1)*50 + 25;
       ret.scaleholder=25;
       ret.wholescale=margin;
       ret.rightarrow=rightarrowpos;
@@ -44,40 +49,40 @@ var Timeline= React.createClass({
       ret.arrowshidden = false;
       ret.scaleholder=25;
       ret.wholescale=0;
-      var totalwidth = ((this.state.end - this.state.start) + 1)*50 + 50;
-      ret.leftshift=this.state.width - totalwidth; 
-      var rightarrowpos = ((this.state.width) - 25) ;
+      var totalwidth = ((end - start) + 1)*50 + 50;
+      ret.leftshift=width - totalwidth; 
+      var rightarrowpos = ((width) - 25) ;
       ret.rightarrow=rightarrowpos;
-      if (this.state.trigger == "scroll") {
+      if (trigger == "scroll") {
         ret.scroll = 0;
-      } else if (this.state.trigger == "resize" || this.state.trigger == "click") {
+      } else if (trigger == "resize" || trigger == "click") {
         // when we resize we reset the scroll to 0
         // this is so that we can centre the indicator on the screen
         ret.scroll = 0;
 
         // now tweak left shift to centre the indicator on the screen if possible
-        var ch = ((this.state.pos - this.state.start) + 1)*50;
+        var ch = ((pos - start) + 1)*50;
         // adding the arrows
         // if the choice has fallen off the edges if we start the scale from the
         // beginning
-        if (ch > (this.state.width-50)/2) {
+        if (ch > (width-50)/2) {
           /* 
            * when you have enough data to the right of position to move the selected node 
            * to the middle of the screen, then do that. Otherwise move it as close to the
            * middle as possible.
            */
-           if (((totalwidth-50) - ch) > ((this.state.width - 50)/2)) {
+           if (((totalwidth-50) - ch) > ((width - 50)/2)) {
              console.log("GG");
              // first left shift so that the chosen is the right most entry and then
              // left shift by half the size of the screen then add 25 to center
              // the entry on the screen
-             ret.scroll = (((this.state.width - 50)-ch) - ((this.state.width - 50)/2)) + 25;
+             ret.scroll = (((width - 50)-ch) - ((width - 50)/2)) + 25;
              ret.scroll -= ret.leftshift;
            } else {
              console.log("GL");
              //ret.leftshift = 0;
              // not enough space left so just scroll as right as possible
-             ret.scroll = ((this.state.width - 50)-ch) - ((totalwidth - 50) - ch);
+             ret.scroll = ((width - 50)-ch) - ((totalwidth - 50) - ch);
              ret.scroll -= ret.leftshift;
            } 
         } else {
@@ -86,10 +91,10 @@ var Timeline= React.createClass({
            * to the middle of the screen, then do that. Otherwise move it as close to the
            * middle as possible.
            */
-           if ((ch) > ((this.state.width - 50)/2)) {
+           if ((ch) > ((width - 50)/2)) {
              console.log("LG");
              // first right shift until the chosen is the first entry.
-             ret.scroll = ((this.state.width - 50)/2) - ret.leftshift - (ch + 25) + 25;
+             ret.scroll = ((width - 50)/2) - ret.leftshift - (ch + 25) + 25;
            } else {
              console.log("LL");
              ret.scroll = 0 - ret.leftshift;
@@ -105,12 +110,13 @@ var Timeline= React.createClass({
   clicked: function(event) {
     f = $(event.target);
     val = $($(f.parent()[0]).children("span")[0]).text();
+    ret = this.updatePosition('click', this.state.start, this.state.end, parseInt(val), this.state.width)
     this.setState({pos: parseInt(val),
-                   trigger: 'click'});
+                   coords: ret });
   },
   render: function() {
-    var coords=this.updatePosition();
     var lineml = [];
+    var coords = this.state.coords;
     for (var i = this.props.start; i <= this.props.end; i++) {
       classname="scale scnum"+i.toString();
       keyname="scalekey"+i.toString();
@@ -140,8 +146,14 @@ var Timeline= React.createClass({
     </div>
     );
   },
+  leftclick(event) {
+  },
+  rightclick(event) {
+  },
   componentDidMount: function() {
     window.addEventListener('resize', this.handleResize);
+    $(".right-arrow").click(this.rightclick);
+    $(".left-arrow").click(this.leftclick);
   },
 
   handleResize: function(e) {
